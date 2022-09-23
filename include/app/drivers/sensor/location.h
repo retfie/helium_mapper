e2ca657ea579eda219a86c8ca73ec15459af9d70
Kiril Petrov <retfie@gmail.com>
2022-09-23 20:05:06 +0300

    drivers: sensors: custom sensor location channels
    Signed-off-by: Kiril Petrov <retfie@gmail.com>

---
+++ include/app/drivers/sensor/location.h
@@ -0,0 +1,23 @@
+/*
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+#ifndef __ZEPHYR_INCLUDE_ZEPHYR_DRIVERS_SENSOR_LOCATION_H__
+#define __ZEPHYR_INCLUDE_ZEPHYR_DRIVERS_SENSOR_LOCATION_H__
+
+#include <zephyr/drivers/sensor.h>
+
+/** @brief LOCATION custom channels. */
+enum location_channel {
+	/** Latitudal position in decimal degrees (0 to +-180) */
+	SENSOR_CHAN_NAV_LATITUDE = SENSOR_CHAN_PRIV_START,
+	/** Longitudal position in decimal degrees (0 to +-180) */
+	SENSOR_CHAN_NAV_LONGITUDE,
+	/** Altitude */
+	SENSOR_CHAN_NAV_ALTITUDE,
+	/** Relative accuracy of horizontal position */
+	SENSOR_CHAN_NAV_ACCURACY,
+};
+
+#endif /* __ZEPHYR_INCLUDE_ZEPHYR_DRIVERS_SENSOR_LOCATION_H__ */
