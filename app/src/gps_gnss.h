da20788194ba33c1abef5568f0df9ea7c6be5775
Kiril Petrov <retfie@gmail.com>
2025-01-02 15:33:43 +0200

    app: gnss: add trigger enable/disable
    Signed-off-by: Kiril Petrov <retfie@gmail.com>

--- app/src/gps_gnss.h
+++ app/src/gps_gnss.h
@@ -10,9 +10,20 @@
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
 
+enum gps_power_mode_t {
+	GPS_DISABLE = 0,
+	GPS_ENABLE,
+};
+
+enum gps_trig_mode_t {
+	GPS_TRIG_DISABLE = 0,
+	GPS_TRIG_ENABLE,
+};
+
 typedef void (*gnss_fix_cb_t)(const struct gnss_data *data);
 
 int init_gps_gnss(gnss_fix_cb_t cb);
-void gnss_enable(bool enable);
+int gnss_enable(bool enable);
+int gnss_trigger_set(bool enable);
 
 #endif /* __HELIUM_MAPPER_GPS_GNSS_H__ */
